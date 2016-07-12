330e6cc52a83ddb45c1efd9dc8e54b841581d54b
simonggauthierhec <simon.gregoire-gauthier@hec.ca>
2016-07-12 12:00:32 -0400

    ZCII-2263: Ajout d'un spinner de loading

--- hec-root/public/res/js/index.js
+++ hec-root/public/res/js/index.js
@@ -1,9 +1,23 @@
 (function() {
+	var urlHome = "/portal";
+	var useMockMessage = false;
+	var spinner;
+
+	// Execute get request
+	var get = function(url, callback) {
+		if (url.indexOf("announcement") >= 0 && useMockMessage) {
+			callback(mockMessage());
+		} else {
+			$.getJSON(url, callback);
+		}
+	};
+
+	// Message d'acceuil
 	var getMessage = function() {
-		// Message d'acceuil
-		$.getJSON('/direct/announcement/motd.json?n=1', function(data) {
-			if (data.announcement_collection.length > 0) {
+		get("/direct/announcement/motd.json?n=1", function(data) {
+			if (data && data.announcement_collection.length > 0) {
 				var announcement = data.announcement_collection[0];
+
 				$("#dialog").html(announcement.body);
 				$("#dialog").attr('title', announcement.title);
 				$("#dialog").dialog({
@@ -13,23 +27,67 @@
 						of: window
 					}
 				});
+
+				$("#submit").focus();
 			}
 		});
 	};
 
-	getMessage();
+	// Session
+	var checkSession = function() {
+		get("/direct/user/current.json?" + new Date().getTime(), function(data) {
+			if (data && data.id && data.id.length > 0) {
+				goToHome();
+			} else {
+				showLogin();
+			}
+		});
+	};
 
-})();
+	// Redirige à sakai
+	var goToHome = function() {
+		window.location.replace(urlHome);
+	};
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-2831257-12']);
-_gaq.push(['_trackPageview']);
+	// Affiche le login
+	var showLogin = function() {
+		if (spinner) {
+			spinner.stop();
+		}
 
-(function() {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
+		$("#loading").hide();
+		$("#loginform").fadeIn();
+	};
+
+	// Init le loading
+	var initLoading = function() {
+		spinner = new Spinner({
+			lines: 12,
+			top: "16%",
+			color: "#fff",
+			scale: 2
+		}).spin(document.getElementById("spinner"));
+	};
+
+	// Mock un message
+	var mockMessage = function() {
+		return {
+			announcement_collection: [{
+				body: "Some message body",
+				title: "Some message title"
+			}]
+		};
+	};
+
+	// Page init
+	var init = function() {
+		initLoading();
+
+		getMessage();
+
+		checkSession();
+	};
+
+	init();
+
 })();
