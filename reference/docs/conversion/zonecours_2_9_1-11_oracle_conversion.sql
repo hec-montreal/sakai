5d0f83365652e1f5380f1676109fd9a955533fcb
Curtis van Osch <curtis.van-osch@hec.ca>
2016-07-14 16:46:46 -0400

    Supprimer les modifications aux tables LTI parce qu'on les a déjà, et supprimer 2 tables temporaires quand on a fini avec

--- reference/docs/conversion/zonecours_2_9_1-11_oracle_conversion.sql
+++ reference/docs/conversion/zonecours_2_9_1-11_oracle_conversion.sql
@@ -11,30 +11,30 @@
 -- --------------------------------------------------------------------------------------------------------------------------------------
 
 -- BLTI-222
-ALTER TABLE lti_content ADD (pagetitle VARCHAR2(255));
-ALTER TABLE lti_content MODIFY (launch VARCHAR2(1024));
-ALTER TABLE lti_content ADD (consumerkey VARCHAR2(255));
-ALTER TABLE lti_content ADD (secret VARCHAR2(255));
-ALTER TABLE lti_content ADD (settings CLOB);
-ALTER TABLE lti_content ADD (placementsecret VARCHAR2(512));
-ALTER TABLE lti_content ADD (oldplacementsecret VARCHAR2(512));
-ALTER TABLE lti_tools ADD (allowtitle NUMBER(1) DEFAULT '0');
-ALTER TABLE lti_tools ADD (pagetitle VARCHAR2(255));
-ALTER TABLE lti_tools ADD (allowpagetitle NUMBER(1) DEFAULT '0');
-ALTER TABLE lti_tools MODIFY (launch VARCHAR2(1024));
-ALTER TABLE lti_tools ADD (allowlaunch NUMBER(1) DEFAULT '0');
-ALTER TABLE lti_tools ADD (domain VARCHAR2(255));
-ALTER TABLE lti_tools ADD (allowconsumerkey NUMBER(1) DEFAULT '0');
-ALTER TABLE lti_tools ADD (allowsecret NUMBER(1) DEFAULT '0');
-ALTER TABLE lti_tools ADD (allowoutcomes NUMBER(1) DEFAULT '0');
-ALTER TABLE lti_tools ADD (allowroster NUMBER(1) DEFAULT '0');
-ALTER TABLE lti_tools ADD (allowsettings NUMBER(1) DEFAULT '0');
-ALTER TABLE lti_tools ADD (allowlori NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_content ADD (pagetitle VARCHAR2(255));
+--ALTER TABLE lti_content MODIFY (launch VARCHAR2(1024));
+--ALTER TABLE lti_content ADD (consumerkey VARCHAR2(255));
+--ALTER TABLE lti_content ADD (secret VARCHAR2(255));
+--ALTER TABLE lti_content ADD (settings CLOB);
+--ALTER TABLE lti_content ADD (placementsecret VARCHAR2(512));
+--ALTER TABLE lti_content ADD (oldplacementsecret VARCHAR2(512));
+--ALTER TABLE lti_tools ADD (allowtitle NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_tools ADD (pagetitle VARCHAR2(255));
+--ALTER TABLE lti_tools ADD (allowpagetitle NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_tools MODIFY (launch VARCHAR2(1024));
+--ALTER TABLE lti_tools ADD (allowlaunch NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_tools ADD (domain VARCHAR2(255));
+--ALTER TABLE lti_tools ADD (allowconsumerkey NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_tools ADD (allowsecret NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_tools ADD (allowoutcomes NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_tools ADD (allowroster NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_tools ADD (allowsettings NUMBER(1) DEFAULT '0');
+--ALTER TABLE lti_tools ADD (allowlori NUMBER(1) DEFAULT '0');
 
 -- BLTI-208
-ALTER TABLE lti_tools MODIFY (launch NULL);
-ALTER TABLE lti_tools MODIFY (consumerkey NULL);
-ALTER TABLE lti_tools MODIFY (secret NULL);
+--ALTER TABLE lti_tools MODIFY (launch NULL);
+--ALTER TABLE lti_tools MODIFY (consumerkey NULL);
+--ALTER TABLE lti_tools MODIFY (secret NULL);
 -- LSNBLDR-227
 alter table lesson_builder_groups add siteId varchar2(250 char);
 
@@ -1167,6 +1167,10 @@ INSERT INTO SAKAI_REALM_RL_FN (REALM_KEY, ROLE_KEY, FUNCTION_KEY)
       FROM SAKAI_REALM_RL_FN SRRFI
       WHERE SRRFI.REALM_KEY=SRRFD.REALM_KEY AND SRRFI.ROLE_KEY=SRRFD.ROLE_KEY AND SRRFI.FUNCTION_KEY=TMP.FUNCTION_KEY
   );
+
+-- clean up the temp tables
+DROP TABLE PERMISSIONS_TEMP;
+DROP TABLE PERMISSIONS_SRC_TEMP;
 
 -- END SAK-25862 Default permissions for signup, and backfill.
 
