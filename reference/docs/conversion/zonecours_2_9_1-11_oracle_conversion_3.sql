312f8fb0a53517289460b8a74eaf38b581a166b3
Curtis van Osch <curtis.van-osch@hec.ca>
2016-08-08 14:50:57 -0400

    Create OAUTH tables (Sakai 11.1) as the tool is enabled by default

--- reference/docs/conversion/zonecours_2_9_1-11_oracle_conversion_3.sql
+++ reference/docs/conversion/zonecours_2_9_1-11_oracle_conversion_3.sql
@@ -532,3 +532,11 @@ and site_id in (select site_id from sakai_site where type = 'course'));
 insert into sakai_site_tool_property (SITE_ID, TOOL_ID, NAME, VALUE)
 select SITE_ID, TOOL_ID, 'sakai-portal:visible', 'false' from sakai_site_tool where registration = 'sakai.resources' 
 and site_id in (select site_id from sakai_site where type = 'course');
+
+-- SAK-23666/SAK-31603 
+-- (From sakai_11_0-11_1_oracle_conversion.sql)
+create table OAUTH_ACCESSORS (token varchar2(255 char) not null, secret varchar2(255 char), consumerId varchar2(255 char), userId varchar2(255 char), callbackUrl varchar2(255 char), verifier varchar2(255 char), creationDate timestamp, expirationDate timestamp, status number(10,0), type number(10,0), accessorSecret varchar2(255 char), primary key (token));
+create table OAUTH_CONSUMERS (id varchar2(255 char) not null, name varchar2(255 char) not null, description varchar2(255 char), url varchar2(255 char), callbackUrl varchar2(255 char), secret varchar2(255 char) not null, accessorSecret varchar2(255 char), recordModeEnabled number(1,0), defaultValidity number(10,0) not null, primary key (id));
+create table OAUTH_RIGHTS (id varchar2(255 char) not null, accessright varchar2(255 char) not null, primary key (id, accessright));
+create index user_idx on OAUTH_ACCESSORS (userId);
+alter table OAUTH_RIGHTS add constraint FK5992789F74F42154 foreign key (id) references OAUTH_CONSUMERS;
