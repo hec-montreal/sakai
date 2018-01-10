04c4f379453b0de1e6b4fc7691dd68bea8c75b68
Curtis van Osch <curtis.van-osch@hec.ca>
2018-01-10 11:44:23 -0500

    Merge LTI fix (#1)
    * Revert "ZCII-2770: Connecter le portail public directement avec la vue matérialisé psft - enlever catalog_description_manager"
    
    This reverts commit 7600464910740878f8f20690f2d58fb8ed7b47dc.
    
    * ZCII-3176: faire que la job de auto-submit n'utilise que la date de remises en retard si elle est activé, sinon utiliser la date d'échéance
    
    * ajouter un maven profile pour les projets hec et un autre pour faire un build de sakai (sans nos projets)
    
    * ZCII-3232: Ajuster LTI pour que les coordinator-instructor voient toutes les sections
    
    * ZCII-3232: Ajuster LTI pour que les coordinator-instructor voient toutes les sections
    
    * Revert "ZCII-3176: faire que la job de auto-submit n'utilise que la date de remises en retard si elle est activé, sinon utiliser la date d'échéance"
    
    This reverts commit c420f6e00bf257d2a1df5114993c979bb61399e1.
    
    * remove catalog-description-manager
    
    * cleanup changes

--- basiclti/basiclti-common/src/java/org/sakaiproject/basiclti/util/SakaiBLTIUtil.java
+++ basiclti/basiclti-common/src/java/org/sakaiproject/basiclti/util/SakaiBLTIUtil.java
@@ -528,6 +528,8 @@ public class SakaiBLTIUtil {
 		// or finally all sections if they have site.upd on the site's realm
 		if (SecurityService.isSuperUser()) {
 			groups.addAll(site.getGroups());
+		} else if ("Coordinator-Instructor".equalsIgnoreCase(site.getUserRole(user.getId()).getId())){
+			groups.addAll(site.getGroups());
 		} else if (userGroups != null && userGroups.size() > 0) {
 			groups.addAll(userGroups);
 		} else if (SecurityService.unlock("site.upd", site.getReference())) {
