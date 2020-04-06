f73f97fc56b7ec13cca933a3310a76ed6c0d463e
Curtis van Osch <curtis.van-osch@hec.ca>
2020-04-06 16:18:08 -0400

    ZCII-4100: remettre quelques changements perdu dans le merge pour ZCII-4052 (SAK-34373)

--- samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/services/GradingService.java
+++ samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/services/GradingService.java
@@ -948,9 +948,7 @@ public class GradingService
         itemId = itemGrading.getPublishedItemId();
         ItemDataIfc item = (ItemDataIfc) publishedItemHash.get(itemId);
         if (item == null) {
-        	//this probably shouldn't happen
-        	log.error("unable to retrive itemDataIfc for: {}", publishedItemHash.get(itemId));
-        	continue;
+		throw new DataInconsistencyException("Unable to retrieve itemDataIfc for: " + publishedItemHash.get(itemId));
         }
         for (ItemMetaDataIfc meta : item.getItemMetaDataSet())
         {
@@ -1054,10 +1052,9 @@ public class GradingService
         ItemGradingData itemGrading = iter.next();
         itemId = itemGrading.getPublishedItemId();
         ItemDataIfc item = (ItemDataIfc) publishedItemHash.get(itemId);
-      //SAM-1724 it's possible the item is not in the hash -DH
+        //SAM-1724 it's possible the item is not in the hash -DH
         if (item == null) {
-        	log.error("unable to retrive itemDataIfc for: " + publishedItemHash.get(itemId));
-        	continue;
+		throw new DataInconsistencyException("Unable to retrieve itemDataIfc for: " + publishedItemHash.get(itemId));
         }
 
         itemType2 = item.getTypeId();
@@ -1229,7 +1226,7 @@ public class GradingService
     	  data.setFinalScore(totalAutoScore + data.getTotalOverrideScore());
       }
       log.debug("****x6. "+(new Date()).getTime());
-    } catch (GradebookServiceException ge) {
+    } catch (GradebookServiceException | DataInconsistencyException ge) {
       log.error(ge.getMessage(), ge);
       throw ge;
     } 
