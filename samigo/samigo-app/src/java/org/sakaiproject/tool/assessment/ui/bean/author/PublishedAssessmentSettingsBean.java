2c22f0e0c0fa916b68bbd3586bae2d4a71d99c51
Brian Jones <bjones86@uwo.ca>
2020-09-09 14:10:58 +0000

    SAK-43610: samigo > time exceptions > add validation to time limit (#8537)

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/PublishedAssessmentSettingsBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/PublishedAssessmentSettingsBean.java
@@ -37,7 +37,6 @@ import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
@@ -46,7 +45,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.lang3.StringUtils;
 
-import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.tool.assessment.facade.*;
 import org.sakaiproject.site.api.Group;
 import org.sakaiproject.site.api.Site;
@@ -86,6 +84,7 @@ import org.sakaiproject.tool.assessment.ui.listener.util.ContextUtil;
 import org.sakaiproject.tool.assessment.ui.listener.util.TimeUtil;
 import org.sakaiproject.tool.cover.ToolManager;
 import org.sakaiproject.tool.cover.SessionManager;
+import org.sakaiproject.tool.assessment.util.ExtendedTimeValidator;
 import org.sakaiproject.util.ResourceLoader;
 import org.sakaiproject.util.FormattedText;
 import org.springframework.web.context.ContextLoader;
@@ -1663,12 +1662,8 @@ public void setFeedbackComponentOption(String feedbackComponentOption) {
   //Internal to be able to supress error easier
   public void addExtendedTime() {
       ExtendedTime entry = this.extendedTime;
-      if (StringUtils.isBlank(entry.getUser()) && StringUtils.isBlank(entry.getGroup())) {
-          FacesContext context = FacesContext.getCurrentInstance();
-          String errorString = ContextUtil.getLocalizedString("org.sakaiproject.tool.assessment.bundle.AssessmentSettingsMessages", "extended_time_user_and_group_set");
-          context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, errorString, null));
-      }
-      else {
+      FacesContext context = FacesContext.getCurrentInstance();
+      if (ExtendedTimeValidator.validateEntry(entry, context, this)) {
           AssessmentAccessControlIfc accessControl = new AssessmentAccessControl();
           accessControl.setStartDate(this.startDate);
           accessControl.setDueDate(this.dueDate);
