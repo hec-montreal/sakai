8ebb3daebc38a06368df9411b80f72867d4d13ba
Mame Awa Diop <mame-awa.diop@hec.ca>
2021-01-12 14:58:17 -0500

    ZCII-4374: Inclure la patch pour les questions avec résultats partiels - Copier la configuration

--- samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/facade/QuestionPoolFacadeQueries.java
+++ samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/facade/QuestionPoolFacadeQueries.java
@@ -1240,7 +1240,7 @@ public class QuestionPoolFacadeQueries
       item.setAnswerOptionsRichCount(itemData.getAnswerOptionsRichCount());
       item.setAnswerOptionsSimpleOrRich(itemData.getAnswerOptionsSimpleOrRich());
       item.setDescription(itemData.getDescription());
-      item.setIsExtraCredit(itemData.getIsExtraCredit());
+      //item.setIsExtraCredit(itemData.getIsExtraCredit());
       item.setPartialCreditFlag(itemData.getPartialCreditFlag());
 
       item.setItemTextSet(copyItemText(item.getData(), itemData));
