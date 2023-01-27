175c664030ef96880aad19910617df596f5cf9e9
Curtis van Osch <curtis.vo@gmail.com>
2023-01-27 13:33:24 -0500

    ZCII-4788 : strip html from text fields

--- hec-sql/extract_samigo_responses.sql
+++ hec-sql/extract_samigo_responses.sql
@@ -1,10 +1,15 @@
-SELECT spasst.ID, spasst.TITLE, 
-suim.eid,
-spst.title, spst.sequence,
-spitt.TEXT,
---sigt.answertext, sigt.iscorrect, sigt.comments,
-spat.label, spat.text, spat.iscorrect, sigt.AUTOSCORE AS points, spat.score AS possible_points
---SELECT spst.* 
+-- Couldn't find a better way to strip html than multiple regexp_replace
+SELECT spasst.ID, spasst.TITLE, suim.eid, spst.title, spst.sequence, 
+	spit.INSTRUCTION, 
+	REGEXP_REPLACE(REGEXP_REPLACE(regexp_replace(spit.INSTRUCTION, '(<style(.*)<\/style>)', NULL, 1, 0, 'n'), '(<.*?>)', NULL), '^\s*', NULL, 1, 0, 'm') AS INSTRUCTION_SANS_HTML, 
+	spitt.TEXT, 
+	REGEXP_REPLACE(REGEXP_REPLACE(regexp_replace(spitt.TEXT, '(<style(.*)<\/style>)', NULL, 1, 0, 'n'), '(<.*?>)', NULL), '^\s*', NULL, 1, 0, 'm') AS TEXT_SANS_HTML,
+	sigt.answertext, 
+	REGEXP_REPLACE(REGEXP_REPLACE(regexp_replace(sigt.answertext, '(<style(.*)<\/style>)', NULL, 1, 0, 'n'), '(<.*?>)', NULL), '^\s*', NULL, 1, 0, 'm') AS ANSWERTEXT_SANS_HTML,
+	spat.label, 
+	spat.text, 
+	REGEXP_REPLACE(REGEXP_REPLACE(regexp_replace(spat.text, '(<style(.*)<\/style>)', NULL, 1, 0, 'n'), '(<.*?>)', NULL), '^\s*', NULL, 1, 0, 'm') AS TEXT2_SANS_HTML,
+	spat.iscorrect, sigt.AUTOSCORE AS points, spat.score AS possible_points
 FROM SAM_ASSESSMENTGRADING_T sagt, SAM_PUBLISHEDASSESSMENT_T spasst, 
 	SAM_ITEMGRADING_T sigt, SAM_PUBLISHEDITEM_T spit, SAM_PUBLISHEDITEMTEXT_T spitt, 
 	SAM_PUBLISHEDANSWER_T spat, SAKAI_USER_ID_MAP suim,
@@ -17,4 +22,4 @@ AND spit.SECTIONID = spst.SECTIONID
 AND sagt.AGENTID = suim.USER_ID
 AND spasst.id = sagt.PUBLISHEDASSESSMENTID
 AND sagt.PUBLISHEDASSESSMENTID = sat.QUALIFIERID AND sat.FUNCTIONID = 'OWN_PUBLISHED_ASSESSMENT' AND sat.AGENTID = 'MATH20605.H2022'
-ORDER BY spasst.id, suim.eid, spst."SEQUENCE"
+ORDER BY spasst.id, suim.eid, spst."SEQUENCE", spit."SEQUENCE", spitt."SEQUENCE"
